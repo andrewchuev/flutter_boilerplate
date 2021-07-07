0e472524426e1c613471f213b02065c4321b816f
Andrew A. Chuev <andrew.chuev@gmail.com>
2021-07-07 15:11:00 +0300

    Code refactoring

--- lib/themes.dart
+++ lib/themes.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 
 final kLightTheme = ThemeData.light().copyWith(
     brightness: Brightness.dark,
-    accentColor: Colors.amber,
-    primaryColor: Colors.black,
     appBarTheme: AppBarTheme(color: Colors.green));
 
 final kDarkTheme = ThemeData.dark().copyWith(
     brightness: Brightness.light,
-    accentColor: Colors.amber,
-    primaryColor: Colors.white,
     appBarTheme: AppBarTheme(color: Colors.black));
